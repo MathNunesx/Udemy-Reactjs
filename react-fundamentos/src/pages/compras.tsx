35006f0f255ee12c28b5858b1dfef9d9c7c8999d
Matheus Nunes <matheusjobs04@gmail.com>
2025-09-28 15:57:33 -0300

    pages vs app

---
+++ react-fundamentos/src/pages/compras.tsx
@@ -0,0 +1,9 @@
+import { Cabecalho } from "@/components/interface"
+export default function Page (){
+    return (
+        <div>
+            {Cabecalho()}
+            <h1> Compras</h1>
+        </div>
+    )
+}
