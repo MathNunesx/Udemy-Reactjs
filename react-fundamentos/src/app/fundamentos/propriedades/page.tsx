6728ab161a6a6393cec47088168464c5c012edb5
Matheus Nunes <matheusjobs04@gmail.com>
2025-09-30 23:47:25 -0300

    Tag

---
+++ react-fundamentos/src/app/fundamentos/propriedades/page.tsx
@@ -0,0 +1,11 @@
+import { Acomodacao, Cabecalho, Menu } from "@/components/interface";
+
+export default function Page(){
+    return(
+        <div>
+        <Cabecalho/>
+        <hr />
+        <Acomodacao/>
+        </div>
+    )
+}
