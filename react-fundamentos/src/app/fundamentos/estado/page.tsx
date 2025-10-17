a515af2de05024656073fb2d118e8208ec3ae305
Matheus Nunes <matheusjobs04@gmail.com>
2025-10-16 22:42:10 -0300

    Compenente com Estado

--- react-fundamentos/src/app/fundamentos/estado/page.tsx
+++ react-fundamentos/src/app/fundamentos/estado/page.tsx
@@ -1,18 +1,34 @@
 'use client'
 
+import { useState } from "react"
+
 export default function Page(){
 
-    let num = 0
+    // let num = 0
+
+    let [num, setNumero] = useState(0)
     function incrementar(){
-        num += 1
-        console.log("numero: "+ num)
+        // num += 1
+        setNumero( num + 1 )
+        // console.log("numero: "+ num)
+    }
+
+    function decrementar(){
+        // num += 1
+        setNumero( num - 1 )
+        // console.log("numero: "+ num)
     }
     return(
             <div>
                 <h1> Estados </h1>
                 <button 
                 onClick={incrementar}
-                className="bg-blue-950 p-2"> Click here
+                className="bg-blue-950 p-2"> Adicionar
+                </button> <br></br>
+
+                <button 
+                onClick={decrementar}
+                className="bg-blue-950 p-2"> Diminuir
                 </button>
 
                 <p>Número: {num}</p>
