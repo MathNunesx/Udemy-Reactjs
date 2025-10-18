52257cbd7753d007b152ac2bd681bc9a05582bb9
Matheus Nunes <matheusjobs04@gmail.com>
2025-10-17 22:48:00 -0300

    Caixa de texto

---
+++ react-fundamentos/src/app/fundamentos/entrada/page.tsx
@@ -0,0 +1,10 @@
+export default function Entrada(){
+    return(
+        <div className="p-10">
+            <h1> Entrada de Dados</h1>
+            <input className="campo-text"
+             type="text"
+              />
+        </div>
+    )
+}
