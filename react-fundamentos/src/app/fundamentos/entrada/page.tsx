8eeda9944afc52b34e96fe49977380dbeb490d0d
Matheus Nunes <matheusjobs04@gmail.com>
2025-10-20 23:34:13 -0300

    Estado de caixa de texto

--- react-fundamentos/src/app/fundamentos/entrada/page.tsx
+++ react-fundamentos/src/app/fundamentos/entrada/page.tsx
@@ -1,11 +1,29 @@
+'use client'
+import { useState } from "react"
+
 export default function Entrada(){
+
+     const [valor, setValor] = useState('')
+
+     function alterarValor(e: any){
+        console.log(e)
+        setValor(e.target.value)
+     }
+
+     
     return(
         <div className="p-10">
             <h1> Entrada de Dados</h1>
-            <input className="campo-text"
+            <input 
+            value={valor}
+            className="campo-text"
+            // onChange={e => setValor( e.target.value)}
+            onChange={alterarValor}
              type="text"
              
               />
+
+              <h4> Valor digitado: {valor}</h4>
         </div>
     )
 }
