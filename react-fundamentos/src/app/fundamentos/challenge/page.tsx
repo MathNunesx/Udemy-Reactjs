e69943f5b0465d5be69bc3490a5d2d0d6570a778
Matheus Nunes <matheusjobs04@gmail.com>
2025-10-27 23:49:52 -0300

    Desafio

--- react-fundamentos/src/app/fundamentos/challenge/page.tsx
+++ react-fundamentos/src/app/fundamentos/challenge/page.tsx
@@ -3,6 +3,26 @@ import { useState } from "react"
 
 export default function Entrada(){
 
+    const [peso, setPeso] = useState('')
+    const [altura, setAltura] = useState('')
+    const [resultado, setResultado] = useState('')
+
+    function calcularImc(){
+        const  p = parseFloat(peso)
+        const  a = parseFloat(altura)
+
+        const imc = p / (a * a)
+
+        if(imc <= 18 ){
+            setResultado("Abaixo do peso")
+        } else if(imc > 18 && imc < 25){
+            setResultado("Peso Normal")
+        } else {
+            setResultado("Acima do Peso")
+        }
+
+    }
+
      
     return(
         <div className="p-10">
@@ -12,6 +32,8 @@ export default function Entrada(){
             <p>Digite o seu peso</p>
             <input 
 
+            value={peso}
+            onChange={e=>setPeso(e.target.value)}
             placeholder="ex: 90"
             className="campo-text"
              type="text"
@@ -21,18 +43,22 @@ export default function Entrada(){
              <p>Digite a sua altura</p>
             <input 
 
+            value={altura}
+            onChange={e=>setAltura(e.target.value)}
             placeholder="ex: 1.85"
             className="campo-text"
              type="text"
              /> Metros
              <br /> <br /> <br />
 
-             <button className="botao">
+             <button 
+             onClick={calcularImc}
+             className="botao">
                  Calcular
                   </button>
                   <br />
 
-                  <span> Resultado: </span>
+                  <span> Resultado: {resultado}</span>
 
         </div>
     )
